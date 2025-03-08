d0fd83bb1261cfa10f04056093a22d9fdc88ba31
Deniz Özaltay <denizozaltay2003@hotmail.com>
2025-03-08 14:25:29 +0300

    odev5
    Next.js’e Giriş

---
+++ odev5/src/pages/index.jsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Merhaba Next.js!</h1>
+      <Link href="/about">Hakkımızda</Link>
+    </div>
+  );
+}
